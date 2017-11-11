c950bac356a9127cc640c4d6eb8469208277a9e7
GreyceSouza <greyce.c.s.s@hotmail.com>
2017-11-10 22:22:38 -0200

    Add files via upload

---
+++ Tia30870984/Tia30870984/src/tia30870984/Calculos.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tia30870984;
+
+/**
+ *
+ * @author 30870984
+ */
+public class Calculos {
+    public float calc(int[] digitos, int modo) {
+        switch(modo)
+        {
+            case 1:
+            
+                break;
+            
+            case 2:
+            case 3:
+                
+            break;
+        }
+        
+        return 0;
+  }
+}
